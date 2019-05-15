271c870f5feb5ec972dee25af6f8f0dcb9ca417a
SeiichiN <billie175@gmail.com>
2019-05-16 08:19:07 +0900

    ZenHanFormat ややこしい

--- ZenHanFormat.java
+++ ZenHanFormat.java
@@ -10,17 +10,27 @@ public class ZenHanFormat {
         // getByteLength -- utf-8の場合、一文字3バイトとなる。
         int byteDiff = (getByteLength(target, Charset.forName("UTF-8")) - target.length()) / 2;
         int byteNum = getByteLength(target, Charset.forName("UTF-8"));
-        System.out.println("byteNum= " + byteNum);
-                                    
-        int size = length - byteDiff;
+        // System.out.println("target.length= " + target.length());
+        System.out.println("byteDiff= " + byteDiff);
+        System.out.println("byteDiff/2= " +( byteDiff / 2));
+        System.out.println("byteDiff%2= " + (byteDiff % 2));
+
+        String returnText = null;
+        // int size = length - byteDiff;  // org
+        if ((target.length() + byteDiff) >= length) {
+            length = length - ((byteDiff / 2) + (byteDiff % 2)); 
+            returnText = String.format("%." + length + "s", target);
+        } else {
+            returnText = String.format("%-" + length + "s", target);
+        }
         // sizeがマイナスのときって、あるんやろか？  added by Seiichi Nukayama
-        if (size < 0) { size = size * -1; }
+        // if (size < 0) { size = size * -1; }
         // if (size == 0) { size = 1; }
         // もし target文字列が size より大きければ、切り詰める  added by Seiichi Nukayama
-        if (size < target.length()) {
-            target = target.substring(0, size);
-        }
-        return String.format("%-" + size + "s", target);
+        // if (size < target.length()) {
+        //     target = target.substring(0, size);
+        // }
+        return returnText;
     }
 
     private int getByteLength (String string, Charset charset) {
@@ -28,7 +38,8 @@ public class ZenHanFormat {
     }
 
     public static void main (String [] args) {
-        String title = "沖縄タイムスしまパス";
+        String title = "ddssaaff谷山";
+        // String title = "Okinawa Times";
         ZenHanFormat zhf = new ZenHanFormat();
         String text = zhf.zhFormat (title, 10);
         System.out.println (text);
