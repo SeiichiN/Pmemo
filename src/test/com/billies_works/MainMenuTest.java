a1d68a7baf64aa80a2d8462fc5805165121f5cb7
SeiichiN <billie175@gmail.com>
2020-11-20 12:16:11 +0900

    少し変更

--- src/test/com/billies_works/MainMenuTest.java
+++ src/test/com/billies_works/MainMenuTest.java
@@ -7,8 +7,8 @@ public class MainMenuTest {
     public static void main( String[] args ) {
         MainMenu mainMenu = new MainMenu();
         int menuNum = mainMenu.get();
-        System.out.println("選択した番号: " + menuNum);
+        System.out.println("選択した番号:ddd> " + menuNum);
     }
 }
 
-// 修正時刻: Sun Nov 15 08:41:53 2020
+// 修正時刻: Fri Nov 20 12:15:39 2020
